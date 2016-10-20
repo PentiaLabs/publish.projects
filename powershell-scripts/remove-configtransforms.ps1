eb9d7b5d384d233ddbfa49ff2cc0915f5fd79053
Emil Okkels Klein <eok@pentia.dk>
2016-10-20 08:31:57 +0200

    fixed entry point for remove-configtransforms.ps1 to work with gulp

--- powershell-scripts/remove-configtransforms.ps1
+++ powershell-scripts/remove-configtransforms.ps1
@@ -1,8 +1,7 @@
-function remove-configtransforms(
+param(
     [Parameter(Mandatory=$true)]
     [string]$webRootPath)
-{
-    . $PSScriptRoot\DeleteIfConfigTransformFile.ps1
-    Write-Host 'Preparing to delete config transformation files in $webRootPath:' $webRootPath
-    Get-ChildItem $webRootPath -Recurse | Where-Object { $_.Name -like "*.config" } | ForEach-Object { DeleteIfConfigTransformFile ($_.FullName) }
-}
+
+. $PSScriptRoot\DeleteIfConfigTransformFile.ps1
+Write-Host 'Preparing to delete config transformation files in $webRootPath:' $webRootPath
+Get-ChildItem $webRootPath -Recurse | Where-Object { $_.Name -like "*.config" } | ForEach-Object { DeleteIfConfigTransformFile ($_.FullName) }
