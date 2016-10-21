e06876b85ef64f431ec82ca60e63218d9ab297dc
Emil Okkels Klein <eok@pentia.dk>
2016-10-21 12:32:59 +0200

    readded foreach for running publish sequential

--- modules/publish.js
+++ modules/publish.js
@@ -18,7 +18,9 @@ Publish.prototype.publishProjects = function (location, dest) {
 
   console.log("publish to " + dest + " folder");
   return gulp.src([location + "/**/*.csproj", "!" + location + "/**/*Tests.csproj", +location + "/**/*Test.csproj", "!" + location + "/**/*Specflow.csproj"])
-    .pipe(debug({ title: "Building project:" }))
+    .pipe(foreach(function (stream, file) {
+      return stream
+        .pipe(debug({ title: "Building project:" }))
         .pipe(msbuild({
           targets: ["WebPublish"],
           configuration: build.config.name,
@@ -37,6 +39,7 @@ Publish.prototype.publishProjects = function (location, dest) {
             _FindDependencies: "false"
           }
         }));
+    }));
 };
 
 exports = module.exports = new Publish();
