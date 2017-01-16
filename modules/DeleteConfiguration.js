80090111701948149620d00964775dd06f70ee40
Emil Okkels Klein <eok@pentia.dk>
2017-01-16 16:28:57 +0100

    add module for exposing delete configuration functionality

---
+++ modules/DeleteConfiguration.js
@@ -0,0 +1,24 @@
+var gulp = require('gulp');
+var build = require("../build.js");
+var foreach = require("gulp-foreach");
+var msbuild = require("gulp-msbuild");
+var debug = require("gulp-debug");
+var path = require("path");
+
+function DeleteConfiguration () {
+}
+
+DeleteConfiguration.prototype.delete = function () {
+  build.logEvent("builder", "removing configuration transforms");
+  var psFile = path.join(path.dirname(fs.realpathSync(__filename)), "../powershell-scripts/remove-configtransforms.ps1");
+  var websiteRoot = build.config.websiteRoot;
+
+  if(!path.isAbsolute(websiteRoot))
+  {
+    websiteRoot = path.join(process.cwd(),websiteRoot);
+  }
+
+  powershell.runAsync(psFile, " -webRootPath " + websiteRoot, callback);
+};
+
+exports = module.exports = new DeleteConfiguration();
